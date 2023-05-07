cf7a3657bc2747bf8d21cf25b56194db9013a1e2
Tristan Derez <Tristan.derez@gmail.com>
2023-05-07 22:28:48 +0200

    feat: create search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { FormEvent, useState } from "react";
+
+export const SearchBar = () => {
+  const [input, setInput] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const url = `https://google.com/search?q=${input}`;
+    window.location.href = url;
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label='Search'
+            id='fullWidth'
+            onChange={handleChange}
+            color='warning'
+          />
+        </form>
+      </Box>
+    </div>
+  );
+};
